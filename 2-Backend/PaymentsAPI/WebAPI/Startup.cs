1abafd1a5240dc6cfdd6bfdeb634f36d72ed24bf
Cezeta <julic206@gmail.com>
2023-11-28 11:21:15 +1100

    Moved Cors config to Cz.Common

--- 2-Backend/PaymentsAPI/WebAPI/Startup.cs
+++ 2-Backend/PaymentsAPI/WebAPI/Startup.cs
@@ -9,18 +9,12 @@ namespace PaymentsAPI.WebAPI;
 
 public class Startup
 {
+    public IConfiguration Configuration { get; }
+    private CorsConfigHelper _corsConfigHelper;
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    private class CorsConfiguration
-    {
-        public string AllowedOrigins { get; set; }
-        public string AllowedMethods { get; set; }
-        public string AllowedHeaders { get; set; }
+        _corsConfigHelper = new CorsConfigHelper();
     }
 
     // This method gets called by the runtime. Use this method to add services to the container.
@@ -49,59 +43,7 @@ public class Startup
         });
 
         services.AddHttpContextAccessor();
-
-        // Checking if a CORS configuration was provided
-        if (Configuration.GetChildren().Any(item => item.Key == "CORS"))
-            services.AddCors(options => {
-                // Getting all the CORS policies provided
-                var policies = Configuration.GetSection("CORS").GetChildren().ToList();
-                foreach (var policy in policies)
-                {
-                    var policyInformation = Configuration.GetSection(policy.Path).GetChildren();
-                    CorsConfiguration policyConfiguration = new CorsConfiguration();
-                    Configuration.GetSection(policyInformation.First().Path).Bind(policyConfiguration);
-                    // Creating each policy 
-                    options.AddPolicy(policyInformation.First().Key,
-                    builder =>
-                    {
-                        if (string.IsNullOrEmpty(policyConfiguration.AllowedOrigins))
-                            throw new Exception($"CORS Origins AppSetting is null or empty: AllowedOrigins");
-                        else if (policyConfiguration.AllowedOrigins == "*")
-                            builder.AllowAnyOrigin();
-                        else
-                        {
-                            foreach (var origin in policyConfiguration.AllowedOrigins.Split(','))
-                            {
-                                builder.WithOrigins(origin.Trim());
-                            }
-                        }
-
-                        if (string.IsNullOrEmpty(policyConfiguration.AllowedHeaders))
-                            throw new Exception($"CORS Origins AppSetting is null or empty: AllowedHeaders");
-                        else if (policyConfiguration.AllowedHeaders == "*")
-                            builder.AllowAnyHeader();
-                        else
-                        {
-                            foreach (var header in policyConfiguration.AllowedHeaders.Split(','))
-                            {
-                                builder.WithHeaders(header.Trim());
-                            }
-                        }
-
-                        if (string.IsNullOrEmpty(policyConfiguration.AllowedMethods))
-                            throw new Exception($"CORS Origins AppSetting is null or empty: AllowedMethods");
-                        else if (policyConfiguration.AllowedMethods == "*")
-                            builder.AllowAnyMethod();
-                        else
-                        {
-                            foreach (var method in policyConfiguration.AllowedMethods.Split(','))
-                            {
-                                builder.WithMethods(method.Trim());
-                            }
-                        }
-                    });
-                }
-            });
+        services = _corsConfigHelper.ConfigureCors(services, Configuration);
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
