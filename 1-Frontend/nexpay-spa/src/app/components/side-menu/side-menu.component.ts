07cc99c01b69d02541afca36dbc9d5fba0aceb9f
Cezeta <julic206@gmail.com>
2023-12-02 17:15:21 +1100

    Added admin recognition

--- 1-Frontend/nexpay-spa/src/app/components/side-menu/side-menu.component.ts
+++ 1-Frontend/nexpay-spa/src/app/components/side-menu/side-menu.component.ts
@@ -9,12 +9,14 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { UserLoginComponent } from '../user-profile/user-login/user-login.component';
 // Other
 import { RoutesEnum } from '../../app.routes';
+import { UserAuthorityEnum, authenticator } from '../../auth/authenticator';
 
 interface MenuOption {
   id: number;
   name: string;
   icon: string;
   route: string;
+  needsAdmin?: boolean;
 }
 
 @Component({
@@ -36,7 +38,7 @@ interface MenuOption {
   `,
 })
 export class SideMenuComponent implements OnInit {
-  protected menuItems: MenuOption[] = [
+  protected allMenuItems: MenuOption[] = [
     {
       id: 0,
       name: 'Contracts',
@@ -48,14 +50,23 @@ export class SideMenuComponent implements OnInit {
       name: 'Admin',
       icon: 'control',
       route: RoutesEnum.Admin,
+      needsAdmin: true,
     },
   ];
+  protected menuItems: MenuOption[] = [];
   protected currentSection: number = 0;
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     // TODO: OnInit, implement route recognition, and select menu item accordingly
+    authenticator.onLoginSuccess.subscribe(() => {
+      if (authenticator.getUserAuthority() === UserAuthorityEnum.User) {
+        this.menuItems = [...this.allMenuItems.filter((mI) => !mI.needsAdmin)];
+      } else {
+        this.menuItems = [...this.allMenuItems];
+      }
+    });
   }
 
   protected onMenuRedirect(item: MenuOption) {
