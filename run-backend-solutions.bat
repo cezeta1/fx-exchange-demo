7f5bf7d9952247c939db8e0a9a7e97b9aa987752
Julian Czerweny <julian.czerweny@nexpay.com.au>
2025-04-09 12:06:08 +1000

    denameified rest

--- run-backend-solutions.bat
+++ run-backend-solutions.bat
@@ -1,3 +1,3 @@
 start   cmd.exe /C "cd 2-Backend\FXRatesAPI\WebAPI & title FXRatesAPI & dotnet run -lp https"
 start   cmd.exe /C "cd 2-Backend\PaymentsAPI\WebAPI & title PaymentsAPI & dotnet run -lp https"
-start   cmd.exe /C "cd 2-Backend\NexPayBFF\WebAPI & title CezetaBFF & dotnet run -lp https"
+start   cmd.exe /C "cd 2-Backend\CezetaBFF\WebAPI & title CezetaBFF & dotnet run -lp https"
